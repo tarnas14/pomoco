5ee3522d331d2b276d770eb7eb076f41a563893e
Matt Stasch <matt.stasch@gmail.com>
2016-07-05 12:49:25 +0200

    First working version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const util    = require('util');
 const process = require('process');
-const asciimo = require('asciimo');
+const asciimo = require('asciimo').Figlet;
 const moment  = require('moment');
 const clear   = require('clear');
+const colors  = require('colors');
 
 const REFRESH_RATE = 250; // ms
 
@@ -15,28 +17,57 @@ process.stdout.on('resize', () => {
   HEIGHT = process.stdout.rows;
 });
 
-let POMODORO  = 0.1; // m
-let BREAK     = 0.1; // m
-
-let pend = moment().add(POMODORO, 'm');
-let bend = moment().add(POMODORO + BREAK, 'm');
-
 const formatTime = (a,b) => {
   const t = moment(b.diff(a));
 
   return t.format('mm:ss');
 };
 
+const render = (time, color) => {
+  clear();
+  asciimo.write(time, FONT, (art) => {
+    clear();
+    process.stdout.write('\r\n');
+    util.puts(color === 'red' ? art.red : art.green); 
+    asciimo.write(time, FONT, (art) => {
+
+    });
+  });
+};
+
+const parseArgs = (idx, def) => {
+  if (process.argv.length <= (2 + idx)) {
+    return def;
+  }
+
+  return process.argv[2 + idx];
+};
+
+// -----------------------------------------------------
+
+let POMODORO  = parseArgs(0, 25); // m
+let BREAK     = parseArgs(1, 5);  // m
+let FONT = parseArgs(2, 'Colossal');
+
+// -----------------------------------------------------
+let pend = moment().add(POMODORO, 'm');
+let bend = moment().add(POMODORO + BREAK, 'm');
+
 const timer = setInterval(() => {
   const now = moment();
 
   if (now.isBefore(pend)) {
-    console.log('P', formatTime(now, pend));
+    render(
+        formatTime(now, pend),
+        'red'
+    );
   } else if(now.isBefore(bend)) {
-    console.log('B', formatTime(now, bend));
+    render(
+        formatTime(now, bend),
+        'green'
+    );
   } else {
-    clearInterval(timer);
-    console.log('See ya!');
+    pend = moment().add(POMODORO, 'm');
+    bend = moment().add(POMODORO + BREAK, 'm'); 
   }
-
 }, REFRESH_RATE);
